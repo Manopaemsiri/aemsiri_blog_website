f4336d3dd0d6719deb555606fb9b3093483a1481
Manop Aemsiri <manop.aemsiri@gmail.com>
2023-08-26 22:27:25 +0700

    Validate

--- server/controllers/blogController.js
+++ server/controllers/blogController.js
@@ -4,7 +4,16 @@ const slugify = require("slugify")
 // บันทึกข้อมูล
 exports.create=(req,res)=>{
   const {title, content, author}=req.body
-  const slug = slugify(req.body.slug)
+  const slug = slugify(title)
+
+  // validate ตรวจสอบความถูกต้องของข้อมูล
+  switch(true){
+    case !title: 
+      return res.status(400).json({error:"กรุณาป้อนชื่อบทความ"})
+      break;
+    case !content:
+      return res.status(400).json({error:"กรุณาป้อนเนื้อหาบทความ"})
+  }
   res.json({
     data:{title, content, author, slug}
   })
